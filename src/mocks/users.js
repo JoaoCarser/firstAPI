1a0644c11c1239a533a22f2d306a80f2c369a389
João Carser <joaogabriel-14@hotmail.com>
2024-01-30 20:28:01 -0300

    Inicializado API e primeira rota

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: '1',
+        name: 'Joao',
+    },
+    {
+        id: '2',
+        name: 'Carser',
+    },
+    {
+        id: '3',
+        name: 'Done',
+    },
+]
