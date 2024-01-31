33d6a461eaa466372034c699dd631a9fcea9cbff
João Carser <joaogabriel-14@hotmail.com>
2024-01-31 09:35:33 -0300

    feat: :construction: Desestruturando rotas e criando UserController

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-const http = require('http');
-const users = require('./mocks/users');
+const http = require("http");
 
+const routes = require("./routes");
 
 const server = http.createServer((request, response) => {
-    console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
+  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
+  const route = routes.find(
+    (routeObj) =>
+      routeObj.endpoint === request.url && routeObj.method === request.method
+  );
 
-    if (request.url === '/users' && request.method === 'GET') {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.end(JSON.stringify(users));
-    } else {
-        response.writeHead(404, { 'Content-Type': 'text/html' });
-        response.end(`Cannot ${request.method} -- ${request.url}`);
-    }
+  
+  if (route) {
+    route.handler(request, response);
+  } else {
+    response.writeHead(404, { "Content-Type": "text/html" });
+    response.end(`Cannot ${request.method}  ${request.url}`);
+  }
 });
 
-
-
-
 server.listen(3000, () => {
-    console.log('🔥 Server started at http://localhost:3000');
+  console.log("🔥 Server started at http://localhost:3000");
 });
